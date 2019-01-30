be9c5f7dfb032c6cd95d8cab3862d6cfc31d9f02
Richard Adiguna <>
2019-01-31 00:00:39 +0700

    utils/Utils.py: Add store_raw_images function

--- utils/Utils.py
+++ utils/Utils.py
@@ -12,3 +12,19 @@ def get_args():
         help='The Configuration file')
     args = argparser.parse_args()
     return args
+
+
+def store_raw_images(negtives_data_path, write_dir):
+    fnames = os.listdir(negtives_data_path)
+
+    for fname in fnames:
+        full_path = os.path.join(negtives_data_path, fname)
+
+        image = cv2.imread(full_path, cv2.IMREAD_GRAYSCALE)
+        resized_images = cv2.resize(image, (100, 100))
+
+        cv2.imwrite(write_dir + fname, resized_images)
+
+
+def create_pos_n_neg():
+    return
